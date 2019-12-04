a2a9751c31616e2ccc76869ec2c679c8ca966a84
Aaron Langford <alangford@instructure.com>
2019-12-03 21:57:24 -0800

    Enables 3 endpoints for chatting

--- www/index.py
+++ www/index.py
@@ -3,14 +3,18 @@ import os
 from flask import Flask
 from flask import Markup
 from flask import render_template
+from flask import jsonify
+from flask import request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text
+from sqlalchemy import and_
+import time
 
 app = Flask(__name__)
 
 # Configure MySQL connection.
 db = SQLAlchemy()
-db_uri = 'mysql://root:supersecure@db/information_schema'
+db_uri = 'mysql://root:supersecure@db/messaging'
 app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
@@ -35,5 +39,111 @@ def test():
     # Return the page with the result.
     return render_template('index.html', result=result)
 
+@app.route("/thread", methods=['POST'])
+def create_thread():
+    data = request.json
+
+    mysql_result = False
+
+    thread = Thread()
+    
+    for u in data['users']:
+        thread.participants.append(ThreadParticipant(user_id=u))
+
+    try:
+        db.session.add(thread)
+        db.session.commit()
+
+        mysql_result = True
+    except Exception as e:
+        ## TODO: wire up some actual logger implemention
+        print(e)
+        db.session.rollback()
+        db.session.flush()
+        pass
+
+    if mysql_result:
+        result = jsonify({
+            'threadId': thread.id
+        })
+        result.status_code = 201
+    else:
+        result = jsonify({
+            'status': '501',
+            'message': 'Sorry, I\'m not yet smart enough to explain why I failed'
+        })
+        result.status_code = 501
+
+    return result
+
+@app.route("/thread/<int:thread_id>/<user_id>", methods=['POST'])
+def create_message(thread_id, user_id):
+    message = request.json['message']
+
+    mysql_result = False
+
+    ## This is unchecked. Thread could not exist
+    participant = ThreadParticipant.query.filter(
+        and_(ThreadParticipant.thread_id == thread_id, ThreadParticipant.user_id == user_id)
+    ).one()
+
+    participant.messages.append(ThreadMessage(thread_id=thread_id,content=message))
+
+    try:
+        db.session.commit()
+
+        mysql_result = True
+    except Exception as e:
+        ## TODO: wire up some actual logger implemention
+        print(e)
+        db.session.rollback()
+        db.session.flush()
+        pass
+
+    if mysql_result:
+        result = '', 204
+    else:
+        result = jsonify({
+            'status': '501',
+            'message': 'Sorry, I\'m not yet smart enough to explain why I failed'
+        })
+        result.status_code = 501
+
+    return result
+
+@app.route("/thread/<int:thread_id>", methods=['GET'])
+def get_thread(thread_id):
+    ## This is unchecked. Thread could not exist
+    thread = Thread.query.get(thread_id)
+    
+    data = {
+        'messages': list(map(lambda m: {'username': m.user_id, 'message': m.content}, thread.messages))
+    }
+
+    return jsonify(data)
+
+class Thread(db.Model):
+    ## Would look into this being a BigInt...
+    id = db.Column(db.Integer, primary_key=True)
+    participants = db.relationship('ThreadParticipant', backref='thread', lazy=True)
+    messages = db.relationship('ThreadMessage', backref='thread', lazy=True)
+
+
+class ThreadParticipant(db.Model):
+    user_id = db.Column(db.String(255), nullable=False, primary_key=True)
+    thread_id = db.Column(db.Integer, db.ForeignKey('thread.id'), primary_key=True)
+    messages = db.relationship('ThreadMessage', backref='participant', lazy=True)
+
+class ThreadMessage(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    thread_id = db.Column(db.Integer, db.ForeignKey('thread.id'))
+    user_id = db.Column(db.String(255), db.ForeignKey(ThreadParticipant.__tablename__ + '.user_id'))
+    content = db.Column(db.Text, nullable=False)
+
 if __name__ == "__main__":
+    with app.app_context():
+        ## hack to make web app wait for mysql to be ready
+        time.sleep(30)
+        db.create_all()
+
     app.run(host="0.0.0.0", port=80)
